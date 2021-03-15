c8f148a2ef1f4a6f1153df3b25834789b15cf119
Acothran22 <anthony.cothran@yahoo.com>
2021-03-15 13:37:43 -0700

    Phase 2 of independent project

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    private Rigidbody rb;
+    
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal"); 
-        float vertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
-
-        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
         
     }
 
     
-    
-        
-    
+
+
+
 }
