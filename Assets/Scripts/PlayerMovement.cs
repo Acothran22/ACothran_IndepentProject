11b6ae82dd094b4d508e5679b3423842f8cd57a4
Acothran22 <anthony.cothran@yahoo.com>
2021-04-18 19:56:56 -0700

    Project phase 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject winTextObject;
     public float speed;
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
+    private int count;
     
    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >=9)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -26,7 +42,17 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+        
+    }
 
 
 
